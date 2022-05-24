5195949f6e64987204201c75c86f08e7a72a9dd2
AYORINDE TIMILEHIN <timilxr@gmail.com>
2022-05-24 08:42:43 +0100

    second commit

---
+++ scripts/index.js
@@ -0,0 +1,101 @@
+const users = [
+    {
+        name: 'David Wagner',
+        mail: 'david_wagner@example.com',
+        label: 'Super Admin',
+        date: 'Super Admin',
+        role: 'CEO and Founder',
+        img: 'profile.jpg'
+    },
+    {
+        name: 'David Wagner',
+        mail: 'david_wagner@example.com',
+        label: 'Super Admin',
+        date: 'Super Admin',
+        role: 'CEO and Founder',
+        img: 'profile.jpg'
+    },
+    {
+        name: 'David Wagner',
+        mail: 'david_wagner@example.com',
+        label: 'Super Admin',
+        date: 'Super Admin',
+        role: 'CEO and Founder',
+        img: 'profile.jpg'
+    },
+    {
+        name: 'David Wagner',
+        mail: 'david_wagner@example.com',
+        label: 'Super Admin',
+        date: 'Super Admin',
+        role: 'CEO and Founder',
+        img: 'profile.jpg'
+    },
+    {
+        name: 'David Wagner',
+        mail: 'david_wagner@example.com',
+        label: 'Super Admin',
+        date: 'Super Admin',
+        role: 'CEO and Founder',
+        img: 'profile.jpg'
+    },
+    {
+        name: 'David Wagner',
+        mail: 'david_wagner@example.com',
+        label: 'Super Admin',
+        date: 'Super Admin',
+        role: 'CEO and Founder',
+        img: 'profile.jpg'
+    },
+    {
+        name: 'David Wagner',
+        mail: 'david_wagner@example.com',
+        label: 'Super Admin',
+        date: 'Super Admin',
+        role: 'CEO and Founder',
+        img: 'profile.jpg'
+    },
+    {
+        name: 'David Wagner',
+        mail: 'david_wagner@example.com',
+        label: 'Super Admin',
+        date: 'Super Admin',
+        role: 'CEO and Founder',
+        img: 'profile.jpg'
+    },
+    {
+        name: 'David Wagner',
+        mail: 'david_wagner@example.com',
+        label: 'Super Admin',
+        date: 'Super Admin',
+        role: 'CEO and Founder',
+        img: 'profile.jpg'
+    },
+    {
+        name: 'David Wagner',
+        mail: 'david_wagner@example.com',
+        label: 'Super Admin',
+        date: 'Super Admin',
+        role: 'CEO and Founder',
+        img: 'profile.jpg'
+    },
+]
+
+const table = document.getElementsByClassName('contents')[0];
+let row = document.createElement('div');
+let nameElem = document.createElement('div');
+let dateElem = document.createElement('div');
+let roleElem = document.createElement('div');
+let actionElem = document.createElement('div');
+let profileElem = document.createElement('img');
+let nameSectionElem = document.createElement('div');
+let name = document.createElement('h4');
+let email = document.createElement('p');
+let label = document.createElement('p');
+row.classList = 'd-flex align-items-center';
+nameElem.classList = 'd-flex name align-items-center';
+dateElem.classList = 'date';
+roleElem.classList = 'role';
+actionElem.classList = 'action';
+// nameElem.classList = 'd-flex name align-items-center';
+
